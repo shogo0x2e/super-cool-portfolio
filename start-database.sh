ea08bf4eb229657fcbe5d29155d84304b9f64e43
Shogo Kitada <contact@shogo0x2e.com>
2024-02-06 17:55:45 +0900

    start-database.sh の実行

--- start-database.sh
+++ start-database.sh
@@ -9,7 +9,7 @@
 
 # On Lunux and macOS you can run this script directly - `./start-database.sh`
 
-DB_CONTAINER_NAME=".-postgres"
+DB_CONTAINER_NAME="portfolio-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
   echo "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
